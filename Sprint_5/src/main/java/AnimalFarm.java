bb4549595db8d44ce9d0c10ac3798df8e51acd22
Tatyana Melnikova <t-melnikova96@mail.ru>
2023-04-30 16:04:53 +0300

    Исправления после сверки решения с тренажером

--- Sprint_5/src/main/java/AnimalFarm.java
+++ Sprint_5/src/main/java/AnimalFarm.java
@@ -1,73 +1,55 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class AnimalFarm {
-    //Подготовка
-    private final List<String> farmAnimals;
+    private List<String> farmAnimals;
+
     public AnimalFarm(List<String> farmAnimals) {
         this.farmAnimals = farmAnimals;
     }
 
-    //Первое задание
-    public HashMap<String, Integer> countedAnimals() {
-        HashMap<String, Integer> tableAnimal = new HashMap<String, Integer>();
+    public Map<Animal, Integer> countedAnimals() {
+        Map<Animal, Integer> animalMap = new HashMap<>();
 
-        for(int i = 0; i < this.farmAnimals.size(); i++) {
-            String[] splitString = this.farmAnimals.get(i).split(" ");
-            if (splitString[0] != "NOT_DEFINED") {
-                if(!tableAnimal.containsKey(splitString[0])) {
-                    int count = 1;
-                    for (int j = i + 1; j < this.farmAnimals.size(); j++) {
-                        if(this.farmAnimals.get(j) == splitString[0]) {
-                            count = count + 1;
-                        }
-                    }
-                    tableAnimal.put(splitString[0], count);
-                }
-            } else {
-                System.out.println("Please correct string " + Animal.NOT_DEFINED + ". Incorrect input data.");
+        for (String farmAnimal : farmAnimals) {
+            Animal animal;
+            try {
+                animal = Animal.valueOf(farmAnimal.split(" ")[0].toUpperCase());
+                Integer currentNumber = animalMap.get(animal);
+                animalMap.put(animal, currentNumber == null ? 1 : currentNumber + 1);
+            } catch (Exception e) {
+                System.out.printf("Please correct string %s. Incorrect input data. %n", farmAnimal);
             }
         }
-        return tableAnimal;
+        return animalMap;
     }
 
-    //Второе задание
-    public HashSet<String> uniqueNames() {
-        //мы должны взять список, сплитнуть его и вытащить вторую часть и положить его отдельно в список
-        String[] splitFarmAnimals = this.farmAnimals.toString().split(" ");
-        //че делать после разделения, как забрать то, что нужно... пробую так:
-        HashSet<String> listOfUniqueNames = new HashSet<>();
-        for (int i = 0; i < splitFarmAnimals.length; i++) {
-            if (i % 2 != 0) {
-                if(splitFarmAnimals[i] != "N") {
-                    listOfUniqueNames.add(splitFarmAnimals[i]);
-                } else {
-                    System.out.println("Please correct string " + splitFarmAnimals[i] + ". Incorrect input data.");
-                }
+    public Set<String> uniqueNames() {
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (String farmAnimal : farmAnimals) {
+            String name;
+            try {
+                name = farmAnimal.split(" ")[1];
+                uniqueNames.add(name);
+            } catch (Exception e) {
+                System.out.printf("Please correct string %s. Incorrect input data. %n", farmAnimal);
             }
         }
-        //вернуть уникальные имена
-        return listOfUniqueNames;
+        return uniqueNames;
     }
 
-    //Третье задание: методы для разных вариантов добавления нового животного на ферму
+    public void addFarmAnimal(Animal animal, String name) {
+        farmAnimals.add(animal.name() + " " + name);
+    }
 
-    //когда передали и вид, и имя
-    public void addVarOne(String typeOfAnimal, String name) {
-        this.farmAnimals.add(typeOfAnimal + " " + name);
+    public void addFarmAnimal(Animal animal) {
+        farmAnimals.add(animal.name() + " N");
     }
-    //когда передали только вид
-    public void addVarTwo(String typeOfAnimal) {
-        String name = "N";
-        this.farmAnimals.add(typeOfAnimal + " " + name);
+
+    public void addFarmAnimal(String name) {
+        farmAnimals.add(Animal.NOT_DEFINED + " " + name);
     }
-    //когда передали только имя животного
-    public void addVarThree(String name) {
-        String typeOfAnimal = String.valueOf(Animal.NOT_DEFINED);
-        this.farmAnimals.add(typeOfAnimal + " " + name);
-    }
-    //Четвертое задание
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
@@ -75,6 +57,8 @@ public class AnimalFarm {
             String printFarmAnimal = farmAnimal.replace(" ", ":");
             stringBuilder.append(printFarmAnimal).append("\n");
         }
+
         return stringBuilder.toString();
     }
-}
+
+} 
